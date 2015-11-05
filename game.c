4fa36a26a03a67483d314814659cf97baac6c563
nikopapp <np15685@my.bristol.ac.uk>
2015-11-05 11:55:28 +0000

    05-11-2015/11:55

--- game.c
+++ game.c
@@ -3,17 +3,17 @@
 
 /*hello --this is a test change-- */
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
    int sw;
    if(argc==2){
-      sw=atoi(argv[1]);
+      sw = atoi(argv[1]);
    }
    else{
    printf("1.Binary Game\n");
    printf("2.Encryption Game\n");
+   scanf("%d", &sw );
    }
-   scanf("%d",&sw );
 switch (sw) {
   case 1:
      if(binaryGame()==1){printf("everything OK\n");}
