b306291f51fa0048d09b4ce290a12e22849d8d80
nikopapp <np15685@my.bristol.ac.uk>
2015-11-09 13:19:31 +0000

    09-11-2015/13:19

---
+++ input.c
@@ -0,0 +1,100 @@
+#include "input.h"
+
+int input(void)
+{
+  int input_value;
+  int *input_pointer;
+  SDL_Simplewin sw;
+  input_pointer=&input_value;
+  Nikos_SDL_Init(&sw);
+  while(!sw.finished){
+      Nikos_SDL_Events(&sw,input_pointer);
+  }
+  return(input_value);
+}
+
+
+
+void Nikos_SDL_Init(SDL_Simplewin *sw)
+{
+
+
+   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      fprintf(stderr, "\nUnable to initialize SDL:  %s\n", SDL_GetError());
+      SDL_Quit();
+      exit(1);
+   }
+
+   sw->finished = 0;
+
+   sw->win= SDL_CreateWindow("SDL Window",
+                          SDL_WINDOWPOS_UNDEFINED,
+                          SDL_WINDOWPOS_UNDEFINED,
+                          WWIDTH, WHEIGHT,
+                          SDL_WINDOW_SHOWN);
+   if(sw->win == NULL){
+      fprintf(stderr, "\nUnable to initialize SDL Window:  %s\n", SDL_GetError());
+      SDL_Quit();
+      exit(1);
+   }
+
+   sw->renderer = SDL_CreateRenderer(sw->win, -1, 0);
+   if(sw->renderer == NULL){
+      fprintf(stderr, "\nUnable to initialize SDL Renderer:  %s\n", SDL_GetError());
+      SDL_Quit();
+      exit(1);
+   }
+
+   /* Set screen to black
+   Neill_SDL_SetDrawColour(sw, 0, 0, 0);
+   SDL_RenderClear(sw->renderer);
+   SDL_RenderPresent(sw->renderer);*/
+
+}
+/* Gobble all events & ignore most */
+int Nikos_SDL_Events(SDL_Simplewin *sw, int *p)
+{
+   SDL_Event event;
+   while(SDL_PollEvent(&event))
+   {
+      switch (event.type){
+         case SDL_MOUSEBUTTONDOWN:
+         case SDL_KEYDOWN:
+            switch( event.key.keysym.sym ){
+               case SDLK_LEFT:
+                  *p=1;
+                  printf("%d\n",*p);
+                  sw->finished=1;
+                  return(1);
+                  break;
+               case SDLK_RIGHT:
+                  *p=2;
+                  printf("%d\n",*p);
+                  sw->finished=1;
+                  return(2);
+                  break;
+               case SDLK_UP:
+                  *p=3;
+                  printf("%d\n",*p);
+                  sw->finished=1;
+                  return(3);
+                  break;
+               case SDLK_DOWN:
+      //   printf("Interaction\n");
+                  *p=4;
+                  printf("%d\n",*p);
+                  sw->finished=1;
+                  return(4);
+                  break;
+               case SDLK_ESCAPE:
+                  sw->finished =1;
+                  break;
+         }
+         break;
+         case SDL_QUIT:
+            sw->finished = 1;
+      }
+      return(0);
+   }
+   return(0);
+}
---
+++ input.h
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+#include <SDL/SDL.h>
+
+#define WWIDTH    400
+#define WHEIGHT   300
+
+struct SDL_Simplewin {
+   SDL_bool finished;
+   SDL_Window *win;
+   SDL_Renderer *renderer;
+};
+typedef struct SDL_Simplewin SDL_Simplewin;
+
+void Nikos_SDL_Init(SDL_Simplewin *sw);
+int Nikos_SDL_Events(SDL_Simplewin *sw, int *p);
