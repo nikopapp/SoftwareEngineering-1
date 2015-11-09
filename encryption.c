52bc7887175c57ce9d00d2fb522936e348b02416
blaza1990 <ab15236@my.bristol.ac.uk>
2015-11-09 12:25:56 +0000

    09-11-2015/12:25

--- encryption.c
+++ encryption.c
@@ -12,7 +12,7 @@ char enc_vowel();
 char enc_constant();
 void enc_changeRow(char word[LENGTH], int size);
 void enc_change(char word[LENGTH], int size, int game);
-void enc_printGrid(char grid[gridSize][gridSize]); //hello
+void enc_printGrid(char grid[gridSize][gridSize]);
 
 int encryption(void)
 {
