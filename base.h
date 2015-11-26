788a5d06a8b99dd7edff1dcfd606d0f339cf3a1f
Neil Justice <neil-justice@users.noreply.github.com>
2015-11-26 15:26:14 +0000

    a

---
+++ base.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* FORWARD DEDCLARATIONS */
+struct SDL_Simplewin;
+typedef struct SDL_Simplewin SDL_Simplewin;
+
+struct entity;
+typedef struct entity entity;
+
+struct cell;
+typedef struct cell cell;
