fd832709e3305966ae898a82bc7bd482ba3dd3ea
Neil Justice <neil-justice@users.noreply.github.com>
2015-12-15 15:16:21 +0000

    draw func

--- bgame.h
+++ bgame.h
@@ -8,5 +8,6 @@ int binResult(entity *byte[BYTE_L]);
 /* creates a new bulb-switch pair */
 entity *newBulb(cell grid[H][W], int x, int y);
 
-
 void bgameDraw(Display *d, cell grid[H][W], char* instruction, int res );
+
+void init(cell grid[H][W], entity *player);
