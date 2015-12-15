f00e088c99994dd5c361f241d39fed6aa30c16ce
Neil Justice <neil-justice@users.noreply.github.com>
2015-12-15 12:29:42 +0000

    fixed door positions

--- game.c
+++ game.c
@@ -26,8 +26,8 @@ int main(void)
         bgame(d);
 
       }
-      if (grid[player->y][player->x].background == door2||
-          /*grid[player->y][player->x+1].background == door2||
+      if (grid[player->y][player->x].background == door2 /*||
+          grid[player->y][player->x+1].background == door2||
           grid[player->y][player->x+2].background == door2*/) {
         encryption(d);
       //delEntity(grid[8][W-1].background);
