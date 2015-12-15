49e022bed747069c9e85b29ab9f586026d2caa64
Neil Justice <neil-justice@users.noreply.github.com>
2015-12-15 12:28:51 +0000

    fixed door positions

--- bgame.c
+++ bgame.c
@@ -28,7 +28,7 @@ int bgame (Display *d)
   door2 = grid[8][0].background = newEntity(impassable,'&',0,8);
   /* layer of floortiles */
   fillGrid(grid);
-  drawBackground(d,'A');
+  drawBackground(d,1);
   drawEntities(d, grid);
   drawFrame(d, 20);
 
@@ -36,7 +36,7 @@ int bgame (Display *d)
   sprintf(instruction,"Try summing %d", goal);
   printf("try summing %d\n", goal );
   printf("result: %d\n", binResult(byte) );
-  drawBackground(d,'A');
+  
   drawEntities(d, grid);
   sprintf(str, "%d%c",res,'\0');
   assert(str!=NULL);
