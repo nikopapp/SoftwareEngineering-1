75f8037d9286a3b3a984f005c90c252958fc4d1d
Neil Justice <neil-justice@users.noreply.github.com>
2015-12-16 10:18:11 +0000

    wires

--- bgame.c
+++ bgame.c
@@ -114,7 +114,7 @@ entity *newBulb(cell grid[H][W], int x, int y)
   int i;
   
   grid[0][x].background = newEntity(impassable,'[',x,0); 
-  for (i = y - 1; i > 0; i--) {
+  for (i = 1; i < y - 1; i++) {
    grid[i][x].background = newEntity(impassable,'[',x,i); /* wires */
    grid[i][x].background->pointsto = grid[i - 1][x].background;
   }
