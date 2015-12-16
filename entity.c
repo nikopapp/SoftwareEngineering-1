1369ed30596c9598b40b47c6f5702737b2bc2d7f
Neil Justice <neil-justice@users.noreply.github.com>
2015-12-16 14:28:05 +0000

    text display

--- entity.c
+++ entity.c
@@ -72,13 +72,13 @@ void move(cell *c, int x, int y, direction dir, cell grid[H][W]) {
 }
 
 int isEdge(int x, int y) {
-  if ( x > (w - 1)) {
+  if ( x > (W - 1)) {
     return 1;
   }
   if ( x < 0) {
     return 1;
   }
-  if ( y > (W - 1)) {
+  if ( y > (H - 1)) {
     return 1;
   }
   if ( y < 0) {
