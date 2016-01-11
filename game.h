acc4a45f9aceab7f018feca9a56aaf38397c0acc
Neil Justice <neil-justice@users.noreply.github.com>
2016-01-11 13:02:24 +0000

    include guards

--- game.h
+++ game.h
@@ -1,3 +1,6 @@
+#ifndef GAMEH
+#define GAMEH
+
 #include "input.h"
 
 int binaryGame(void);
@@ -8,3 +11,5 @@ int bgame(Display *sw);
 void mediaLoad(Display *d);
 void makeBoundariesLobby(cell grid[H][W]);
 void lobbyDraw(Display *d, cell grid[H][W]);
+
+#endif
